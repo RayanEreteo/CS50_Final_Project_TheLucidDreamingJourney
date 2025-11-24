38770df7fac207dfb1d78668a463b90102e689aa
RayanEreteo <rayabf5@gmail.com>
2025-11-24 15:42:43 +0100

    Added new routes

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/journal")
+def dream_journal():
+    return render_template("journal.html")
+
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
+
+@app.route("/introduction")
+def introduction():
+    return render_template("introduction.html")
