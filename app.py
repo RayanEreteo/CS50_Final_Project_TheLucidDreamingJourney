6fabc74c1c741ae9964b6a6142f06f20472eef06
RayanEreteo <rayabf5@gmail.com>
2025-11-24 20:41:33 +0100

    added param for navbar interactivity

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", tab=0)
 
 @app.route("/dreamjournal")
 def dream_journal():
-    return render_template("journal.html")
+    return render_template("journal.html", tab=1)
 
 @app.route("/quiz")
 def quiz():
-    return render_template("quiz.html")
+    return render_template("quiz.html", tab=2)
 
 @app.route("/introduction")
 def introduction():
-    return render_template("introduction.html")
+    return render_template("introduction.html", tab=3)
