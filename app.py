1cf2b4b1df659154c1c7d138ae8abdc1cacf3917
RayanEreteo <rayabf5@gmail.com>
2025-11-26 14:13:54 +0100

    reordering for better logic.

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", tab=0)
 
+@app.route("/introduction")
+def introduction():
+    return render_template("introduction.html", tab=1)
+
 @app.route("/dreamjournal")
 def dream_journal():
-    return render_template("journal.html", tab=1)
+    return render_template("journal.html", tab=2)
+
 
 @app.route("/quiz")
 def quiz():
-    return render_template("quiz.html", tab=2)
-
-@app.route("/introduction")
-def introduction():
-    return render_template("introduction.html", tab=3)
+    return render_template("quiz.html", tab=3)
